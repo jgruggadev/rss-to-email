165704100b12d5d469ae78196d4603129a048bcf
jgruggadev <jtalbans@gmail.com>
2026-05-13 09:55:05 -0400

    Update feeds.ts

--- src/feeds.ts
+++ src/feeds.ts
@@ -1,10 +1,8 @@
 export const feeds = [
-  // Daring Fireball
-  'https://daringfireball.net/feeds/main',
-
-  // JakeArchibald.com
-  'https://jakearchibald.com/posts.rss',
-
-  // Appjeniksaan
-  'https://appjeniksaan.nl/feed.xml',
+  'https://news.google.com/rss/search?q=(site:reuters.com+OR+site:ft.com+OR+site:wsj.com+OR+site:barrons.com)+(markets+OR+economy+OR+Federal+Reserve)+when:7d&hl=en-US&gl=US&ceid=US:en',
+  'https://news.google.com/rss/search?q=(AI+OR+artificial+intelligence+OR+semiconductor+OR+Nvidia+OR+data+center+OR+hyperscaler)+markets+when:7d&hl=en-US&gl=US&ceid=US:en',
+  'https://news.google.com/rss/search?q=(consumer+OR+retail+OR+spending+OR+jobs+OR+wages+OR+housing+OR+credit)+US+economy+when:7d&hl=en-US&gl=US&ceid=US:en',
+  'https://news.google.com/rss/search?q=(China+OR+tariffs+OR+sanctions+OR+oil+OR+Ukraine+OR+Middle+East+OR+shipping)+markets+when:7d&hl=en-US&gl=US&ceid=US:en',
+  'https://news.google.com/rss/search?q=(industrial+OR+manufacturing+OR+aerospace+OR+defense+OR+Lockheed+OR+Boeing)+markets+when:7d&hl=en-US&gl=US&ceid=US:en',
 ]
+
